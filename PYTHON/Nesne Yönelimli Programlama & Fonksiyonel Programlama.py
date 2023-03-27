ab3b7b9032d3795926260b00f4c5033c9f8bb3d6
SHADOW <recep.42kadir@gmail.com>
2023-03-27 13:52:48 +0300

    .

--- PYTHON/Nesne Yönelimli Programlama & Fonksiyonel Programlama.py
+++ PYTHON/Nesne Yönelimli Programlama & Fonksiyonel Programlama.py
@@ -90,11 +90,139 @@ class Marketing(Employees):
 mar1 = Marketing()
 
 
-'''class Employees_yeni():
+class Employees_yeni():
     def __init__(self, FirstName, LastName, Adress):
         self.FirstName = FirstName
         self.LastName = LastName
         self.Adress = Adress
 ali = Employees_yeni("ali", "veli", "konya")
-print(ali.Adress, ali.FirstName, ali.LastName)'''
+print(ali.Adress, ali.FirstName, ali.LastName)
+
+print("--------------------------------------------")
+
+#Python'da Fonksiyonel Programlama
+
+#Fonksiyonlar dilin bastacidir.
+#(Birinci sinif nesnelerdir)
+#Yan etkisiz fonksiyonlar. (stateless, girdi-cikti)
+#Yuksek seviye fonksiyonlar.
+#Vektorel operasyonlar.
+
+#Yan Etkisiz Fonksiyonlar (Pure Functions)
+
+#Ornek1:Bağımsızlık
+A = 5
+def impure_sum(b):
+    return b + A
+
+def pure_sum(a,b):
+    return a + b
+
+print(impure_sum(3))
+print(pure_sum(4, 7))
+
+#Ornek2: Olumcul yan etkiler
+#00P:
+
+class LineCounter:
+    def __init__(self, filename):
+        self.file = open(filename,"r")
+        self.lines = []
+
+    def read(self):
+        self.lines = [line for line in self.file]
+
+    def count(self):
+        return len(self.lines)
+
+lc = LineCounter("C:/Users/recep/Documents/GitHub/work/PYTHON/deneme.txt")
+
+print(lc.lines)
+print(lc.count())
+
+lc.read()
+
+print(lc.lines)
+print(lc.count())
+
+#FP
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return [line for line in f]
+
+def count(lines):
+    return len(lines)
+
+example_lines = read("C:/Users/recep/Documents/GitHub/work/PYTHON/deneme.txt")
+lines_count = count(example_lines)
+lines_count
+
+print("--------------------------------------------")
+
+#İsimsiz Fonksiyonlar 
+def old_sum(a,b):
+    return a +b
+
+print(old_sum(4,5))
+
+new_sum = lambda a,b : a + b
+print(new_sum(4,5))
+
+sirasiz_liste = [("b",3),("a",8),("d",12),("c",1)] 
+print(sirasiz_liste)
+
+sirali_liste = sorted(sirasiz_liste, key = lambda x: x[1]) 
+print(sirali_liste)
+
+print("--------------------------------------------")
+
+#Vektörel Operasyonlar
+#OOP
+
+a = [1,2,3,4]
+b = [2,3,4,5]
+
+ab = []
+
+for i in range (0, len(a)):
+    ab.append(a[i]*b[i])
+
+print(ab)
+
+#FP
+
+import numpy as np 
+
+a = np.array([1,2,3,4])
+b = np.array([2,3,4,5])
+
+print(a*b)
+
+print("--------------------------------------------")
+
+#map & filter & reduce
+
+liste = [1,2,3,4,5]
+
+for i in liste:
+    print(i+10)
+
+print(list(map(lambda x: x + 10, liste)))
+
+#filter
+
+liste1 = [1,2,3,4,5,6,7,8,9,10]
+
+print(list(filter(lambda x: x % 2 == 0, liste1)))
+
+#reduce
+from functools import reduce
+liste2 = [1,2,3,4]
+
+print(reduce(lambda a,b: a + b, liste2))
+
+print("--------------------------------------------")
+
+#Modül Oluşturma
 
